bab2ac20e4ca1cafced91d7182416f8c0a8716c3
TAGUCHI <taguchi@virtualtech.jp>
2024-01-15 08:57:54 +0000

    Squashed 'docker-overpass/overpass-api-httpd/city-roads/' content from commit 6424d38
    git-subtree-dir: docker-overpass/overpass-api-httpd/city-roads
    git-subtree-split: 6424d38d1d82c02f50abf11d617d5f7579bf5e13

---
+++ src/App.vue
@@ -0,0 +1,506 @@
+<template>
+<div>
+  <find-place v-if='!placeFound' @loaded='onGridLoaded'></find-place>
+  <div id="app">
+    <div v-if='placeFound'>
+      <div class='controls'>
+        <a href="#" class='print-button' @click.prevent='toggleSettings'>Customize...</a>
+        <a href="#" class='try-another' @click.prevent='startOver'>Try another city</a>
+      </div>
+      <div v-if='showSettings' class='print-window'>
+        <h3>Display</h3>
+        <div class='row'>
+          <div class='col'>Colors</div>
+          <div class='col colors c-2'>
+            <div v-for='layer in layers' :key='layer.name' class='color-container'>
+              <color-picker v-model='layer.color' @change='layer.changeColor'></color-picker>
+              <div class='color-label'>{{layer.name}}</div>
+            </div>
+          </div>
+        </div>
+
+        <h3>Export</h3>
+        <div class='row'>
+          <a href='#' @click.prevent='zazzleMugPrint()' class='col'>Onto a mug</a> 
+          <span class='col c-2'>
+            Print what you see onto a mug. <br/>Get a unique gift of your favorite city.
+          </span>
+        </div>
+        <div class='preview-actions message' v-if='zazzleLink || generatingPreview'>
+            <div v-if='zazzleLink' class='padded popup-help'>
+              If your browser has blocked the new window, <br/>please <a :href='zazzleLink' target='_blank'>click here</a>
+              to open it.
+            </div>
+            <div v-if='generatingPreview' class='loading-container'>
+              <loading-icon></loading-icon>
+              Generating preview url...
+            </div>
+        </div>
+        <div class='row'>
+          <a href='#'  @click.prevent='toPNGFile' class='col'>As an image (.png)</a> 
+          <span class='col c-2'>
+            Save the current screen as a raster image.
+          </span>
+        </div>
+        
+        <div class='row'>
+          <a href='#'  @click.prevent='toSVGFile' class='col'>As a vector (.svg)</a> 
+          <span class='col c-2'>
+            Save the current screen as a vector image.
+          </span>
+        </div>
+        <div v-if='false' class='row'>
+          <a href='#' @click.prevent='toProtobuf' class='col'>To a .PBF file</a> 
+          <span class='col c-2'>
+            Save the current data as a protobuf message. For developer use only.
+          </span>
+        </div>
+
+        <h3>About</h3>
+        <div>
+          <p>This website was created by <a href='https://twitter.com/anvaka' target='_blank'>@anvaka</a>.
+          It downloads roads from OpenStreetMap and renders them with WebGL.
+          </p>
+          <p>
+           You can find the entire <a href='https://github.com/anvaka/city-roads'>source code here</a>. 
+           If you love this website you can also <a href='https://www.paypal.com/paypalme2/anvakos/3'>buy me a coffee</a>, but you don't have to!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <editable-label v-if='placeFound' v-model='name' class='city-name' :printable='true' :style='{color: labelColorRGBA}' :overlay-manager='overlayManager'></editable-label>
+  <div v-if='placeFound' class='license printable can-drag' :style='{color: labelColorRGBA}'>data <a href='https://www.openstreetmap.org/about/' target="_blank" :style='{color: labelColorRGBA}'>© OpenStreetMap</a></div>
+  </div>
+</template>
+
+<script>
+import FindPlace from './components/FindPlace';
+import LoadingIcon from './components/LoadingIcon';
+import EditableLabel from './components/EditableLabel';
+import ColorPicker from './components/ColorPicker';
+import createScene from './lib/createScene';
+import GridLayer from './lib/GridLayer';
+import generateZazzleLink from './lib/getZazzleLink';
+import appState from './lib/appState';
+import {getPrintableCanvas, getCanvas} from './lib/saveFile';
+import config from './config';
+import './lib/canvas2BlobPolyfill';
+import bus from './lib/bus';
+import createOverlayManager from './createOverlayManager';
+import tinycolor from 'tinycolor2';
+
+class ColorLayer {
+  constructor(name, color, callback) {
+    this.name = name;
+    this.changeColor = callback;
+    this.color = color;
+  }
+}
+
+export default {
+  name: 'App',
+  components: {
+    FindPlace,
+    LoadingIcon,
+    EditableLabel,
+    ColorPicker
+  },
+  data() {
+    return {
+      placeFound: false,
+      name: '',
+      zazzleLink: null,
+      generatingPreview: false,
+      showSettings: false,
+      settingsOpen: false,
+      labelColor: config.getLabelColor().toRgb(),
+      backgroundColor: config.getBackgroundColor().toRgb(),
+      layers: []
+    }
+  },
+  computed: {
+    labelColorRGBA() {
+      return toRGBA(this.labelColor);
+    }
+  },
+  created() {
+    bus.on('scene-transform', this.handleSceneTransform);
+    bus.on('background-color', this.syncBackground);
+    bus.on('line-color', this.syncLineColor);
+    this.overlayManager = createOverlayManager();
+  },
+  beforeDestroy() {
+    this.overlayManager.dispose();
+    this.dispose();
+    bus.off('scene-transform', this.handleSceneTransform);
+    bus.off('background-color', this.syncBackground);
+    bus.off('line-color', this.syncLineColor);
+  },
+  methods: {
+    dispose() {
+      if (this.scene) {
+        this.scene.dispose();
+        window.scene = null;
+      }
+    },
+    toggleSettings() {
+      this.showSettings = !this.showSettings;
+    },
+    handleSceneTransform() {
+      this.zazzleLink = null;
+    },
+    onGridLoaded(grid) {
+      if (grid.isArea) {
+        appState.set('areaId', grid.id);
+        appState.unset('osm_id');
+        appState.unset('bbox');
+      } else if (grid.bboxString) {
+        appState.unset('areaId');
+        appState.set('osm_id', grid.id);
+        appState.set('bbox', grid.bboxString);
+      }
+      this.placeFound = true;
+      this.name = grid.name.split(',')[0];
+      let canvas = getCanvas();
+      canvas.style.visibility = 'visible';
+
+      this.scene = createScene(canvas);
+      this.scene.on('layer-added', this.updateLayers);
+      this.scene.on('layer-removed', this.updateLayers);
+
+      window.scene = this.scene;
+
+      let gridLayer = new GridLayer();
+      gridLayer.id = 'lines';
+      gridLayer.setGrid(grid);
+      this.scene.add(gridLayer)
+    },
+
+    startOver() {
+      appState.unset('areaId');
+      appState.unsetPlace();
+      appState.unset('q');
+      appState.enableCache();
+
+      this.dispose();
+      this.placeFound = false;
+      this.zazzleLink = null;
+      this.showSettings = false;
+      this.backgroundColor = config.getBackgroundColor().toRgb();
+      this.labelColor = config.getLabelColor().toRgb();
+
+      document.body.style.backgroundColor = config.getBackgroundColor().toRgbString();
+      getCanvas().style.visibility = 'hidden';
+    },
+
+    toPNGFile(e) {
+      scene.saveToPNG(this.name)
+    },
+
+    toSVGFile(e) { 
+      scene.saveToSVG(this.name)
+    },
+
+    updateLayers() {
+      // TODO: This method likely doesn't belong here
+      let newLayers = [];
+      let lastLayer = 0;
+      let renderer = this.scene.getRenderer();
+      let root = renderer.getRoot();
+      root.children.forEach(layer => {
+        if (!layer.color) return;
+        let name = layer.id;
+        if (!name) {
+          lastLayer += 1;
+          name = 'lines ' + lastLayer;
+        }
+        let layerColor = tinycolor.fromRatio(layer.color);
+        newLayers.push(new ColorLayer(name, layerColor, newColor => {
+          this.zazzleLink = null;
+          layer.color = toRatioColor(newColor);
+          renderer.renderFrame();
+          this.scene.fire('color-change', layer);
+        }));
+      });
+
+      newLayers.push(
+        new ColorLayer('background', this.backgroundColor, this.setBackgroundColor),
+        new ColorLayer('labels', this.labelColor, newColor => this.labelColor = newColor)
+      );
+
+      this.layers = newLayers;
+
+      function toRatioColor(c) {
+        return {r: c.r/0xff, g: c.g/0xff, b: c.b/0xff, a: c.a}
+      }
+      this.zazzleLink = null;
+    },
+
+    syncLineColor() {
+      this.updateLayers();
+    },
+
+    syncBackground(newBackground) {
+      this.backgroundColor = newBackground.toRgb();
+      this.updateLayers()
+    },
+    // TODO: I need two background methods?
+    updateBackground() {
+      this.setBackgroundColor(this.backgroundColor)
+      this.zazzleLink = null;
+    },
+    setBackgroundColor(c) {
+      this.scene.background = c;
+      document.body.style.backgroundColor = toRGBA(c);
+      this.zazzleLink = null;
+    },
+
+    zazzleMugPrint() {
+      if (this.zazzleLink) {
+        window.open(this.zazzleLink, '_blank');
+        recordOpenClick(this.zazzleLink);
+        return;
+      }
+
+      this.generatingPreview = true;
+      getPrintableCanvas(this.scene).then(printableCanvas => {
+        generateZazzleLink(printableCanvas).then(link => {
+          this.zazzleLink = link;
+          window.open(link, '_blank');
+          recordOpenClick(link);
+          this.generatingPreview = false;
+        }).catch(e => {
+          this.error = e;
+          this.generatingPreview = false;
+        });
+      });
+    }
+  }
+}
+
+function toRGBA(c) {
+    return `rgba(${c.r}, ${c.g}, ${c.b}, ${c.a})`;
+}
+
+function recordOpenClick(link) {
+  if (typeof ga === 'undefined') return;
+
+  ga('send', 'event', {
+      eventCategory: 'Outbound Link',
+      eventAction: 'click',
+      eventLabel: link
+    });
+}
+</script>
+
+<style lang='stylus'>
+@import('./vars.styl');
+
+#app {
+  margin: 8px;
+  max-height: 100vh;
+  position: absolute;
+  z-index: 1;
+  h3 {
+    font-weight: normal;
+  }
+}
+
+.can-drag {
+  border: 1px solid transparent;
+}
+
+.drag-overlay {
+  position: fixed;
+  background: transparent;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.overlay-active {
+  border: 1px dashed highlight-color;
+}
+.overlay-active.exclusive {
+  border-style: solid;
+}
+
+.controls {
+  height: 48px;
+  background: white;
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+  width: desktop-controls-width;
+  justify-content: space-around;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.2), 0 -1px 0px rgba(0,0,0,0.02);
+
+  a {
+    text-decoration: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: highlight-color;
+    margin: 0;
+    border: 0;
+    &:hover {
+      color: emphasis-background;
+      background: highlight-color;
+    }
+  }
+  a.try-another {
+    flex: 1;
+  }
+
+  a.print-button {
+    flex: 1;
+    border-right: 1px solid border-color;
+    &:focus {
+      border: 1px dashed highlight-color;
+    }
+  }
+}
+
+.col {
+    display: flex;
+    flex: 1;
+    select {
+      margin-left: 14px;
+    }
+  }
+.row {
+  margin-top: 4px;
+  display: flex;
+  flex-direction: row;
+  min-height: 32px;
+}
+.colors {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  .color-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 64px;
+  }
+
+  .color-label {
+    font-size: 12px;
+  }
+}
+
+a {
+  border: 1px solid transparent;
+  margin: -1px;
+  text-decoration: none;
+  color: highlight-color
+}
+a:focus {
+  border: 1px dashed highlight-color;
+  outline: none;
+}
+.print-window {
+  max-height: calc(100vh - 48px);
+  overflow-y: auto;
+  border-top: 1px solid border-color;
+  background: white;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.2);
+  width: desktop-controls-width;
+  padding: 8px;
+  .row a {
+    margin-right: 4px;
+  }
+
+  h3 {
+    margin: 8px 0;
+    text-align: right;
+  }
+}
+
+.message {
+  border-top: 1px solid border-color
+  border-bottom: 1px solid border-color
+  background: #F5F5F5;
+}
+
+.preview-actions {
+  display: flex;
+  padding: 8px 0;
+  margin-left: -8px;
+  margin-bottom: 14px;
+  margin-top: 1px;
+  width: desktop-controls-width;
+  flex-direction: column;
+  align-items: stretch;
+  font-size: 14px;
+  align-items: center;
+  display: flex;
+
+  .popup-help {
+    text-align: center;
+  }
+}
+
+.city-name {
+  position: absolute;
+  right: 32px;
+  bottom: 54px;
+  font-size: 24px;
+  color: #434343;
+  input {
+    font-size: 24px;
+  }
+}
+
+.license {
+  text-align: right;
+  position: fixed;
+  font-family: labels-font;
+  right: 32px;
+  bottom: 32px;
+  font-size: 12px;
+  padding-right: 8px;
+  a {
+    text-decoration: none;
+    display: inline-block;
+  }
+}
+
+.c-2 {
+  flex: 2
+}
+
+@media (max-width: small-screen) {
+  #app {
+    width: 100%;
+    margin: 0;
+
+    .preview-actions,.error,
+    .controls, .print-window {
+      width: 100%;
+    }
+    .loading-container {
+      font-size: 12px;
+    }
+
+    .print-window {
+      font-size: 14px;
+    }
+
+  }
+  .city-name  {
+    right: 8px;
+    bottom: 24px;
+  }
+  .license  {
+    right: 8px;
+    bottom: 8px;
+  }
+}
+
+</style>
---
+++ src/lib/GridLayer.js
@@ -0,0 +1,168 @@
+import config from '../config';
+import tinycolor from 'tinycolor2';
+
+const wgl = require('w-gl');
+let counter = 0;
+
+export default class GridLayer {
+  get color() {
+    return this._color;
+  }
+
+  set color(unsafeColor) {
+    let color = tinycolor(unsafeColor);
+    this._color = color;
+    if (this.lines) {
+	this.lines.color = toRatioColor(color.toRgb());
+    }
+    if (this.scene) {
+      this.scene.renderFrame();
+    }
+  }
+
+  get lineWidth() {
+    return this._lineWidth;
+  }
+
+  set lineWidth(newValue) {
+    this._lineWidth = newValue;
+    if (!this.lines || !this.scene) return;
+
+    this.lines.setLineWidth(newValue);
+  }
+
+  constructor() {
+    this._color = config.getDefaultLineColor();
+    this.grid = null;
+    this.lines = null;
+    this.scene = null;
+    this.dx = 0;
+    this.dy = 0;
+    this.scale = 1;
+    this.hidden = false;
+    this.id = 'paths_' + counter;
+    this._lineWidth = 1;
+    counter += 1;
+  }
+
+  getGridProjector() {
+    if (this.grid) return this.grid.projector;
+  }
+
+  getQueryBounds() {
+    const {grid} = this;
+    if (grid) {
+      if (grid.queryBounds) return grid.queryBounds;
+      if (grid.isArea) return {
+        areaId: grid.id
+      };
+    }
+  }
+
+  setGrid(grid) {
+    this.grid = grid;
+    if (this.scene) {
+      this.bindToScene(this.scene);
+    }
+  }
+
+  getViewBox() {
+    if (!this.grid) return null;
+
+    let {width, height} = this.grid.getProjectedRect();
+    let initialSceneSize = Math.max(width, height) / 4;
+    return {
+      left:  -initialSceneSize,
+      top:    initialSceneSize,
+      right:  initialSceneSize,
+      bottom: -initialSceneSize,
+    };
+  }
+
+  moveTo(x, y = 0) {
+    console.warn('Please use moveBy() instead. The moveTo() is under construction');
+    // this.dx = x;
+    // this.dy = y;
+
+    // this._transferTransform();
+  }
+
+  moveBy(dx, dy = 0) {
+    this.dx = dx;
+    this.dy = dy;
+
+    this._transferTransform();
+  }
+
+  buildLinesCollection() {
+    if (this.lines) return this.lines;
+
+    let grid = this.grid;
+    let lines = new wgl.WireCollection(grid.wayPointCount, {
+      width: this._lineWidth,
+      allowColors: true,
+      is3D: false
+    });
+    grid.forEachWay(function(from, to) {
+      lines.add({from, to});
+    });
+    lines.id = this.id;
+
+    this.lines = lines;
+  }
+
+  destroy() {
+    if (!this.scene || !this.lines) return;
+
+    // TODO: This should remove the grid layer too. Need to clean up how
+    // scene interacts with grid layers.
+    this.scene.removeChild(this.lines);
+  }
+
+  bindToScene(scene) {
+    if (this.scene && this.lines) {
+      console.error('You seem to be adding this layer twice...')
+    }
+
+    this.scene = scene;
+    if (!this.grid) return;
+
+    this.buildLinesCollection();
+
+    if (this.hidden) return;
+    this.scene.appendChild(this.lines);
+  }
+
+  hide() {
+    if (this.hidden) return;
+    this.hidden = true;
+    if (!this.scene || !this.grid) return;
+
+    this.scene.removeChild(this.lines);
+  }
+
+  show() {
+    if (!this.hidden) return;
+    this.hidden = false;
+    if (!this.scene || !this.grid) {
+      console.log('Layer will be shown when grid is available');
+      return;
+    }
+
+    this.scene.appendChild(this.lines);
+  }
+
+  _transferTransform() {
+    if (!this.lines) return;
+
+    this.lines.translate([this.dx, this.dy, 0]);
+    this.lines.updateWorldTransform(true);
+    if (this.scene) {
+      this.scene.renderFrame(true);
+    }
+  }
+}
+
+function toRatioColor(c) {
+  return {r: c.r/0xff, g: c.g/0xff, b: c.b/0xff, a: c.a}
+}

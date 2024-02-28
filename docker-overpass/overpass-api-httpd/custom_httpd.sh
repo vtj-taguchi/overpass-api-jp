7e852bf950e8b029b67cbb0f64aecbd1a91dda45
TAGUCHI Takahisa <taguchi@virtualtech.jp>
2024-02-28 10:28:51 +0000

    Add apache-exporter

--- docker-overpass/overpass-api-httpd/custom_httpd.sh
+++ docker-overpass/overpass-api-httpd/custom_httpd.sh
@@ -11,13 +11,17 @@ echo '    SetEnv OVERPASS_DB_DIR "/overpass/db/"' >>_
 echo '</Directory>' >>_
 
 echo -n -e '\n' >>_
-echo 'Include conf/ssl-overpass-osm-jp.conf' >>_
-echo 'Include conf/rewrite.conf' >>_
+echo 'Include conf/server-status.conf' >>_
+
+ENVIRONMENT=$(grep -v -e '^\s*#' -e '^\s*$' ./environment.txt)
+if [ ${ENVIRONMENT} != "TEST" ]; then
+  echo 'Include conf/ssl-overpass-osm-jp.conf' >>_
+  echo 'Include conf/rewrite.conf' >>_
+  sed -i 's/#LoadModule ssl_module/LoadModule ssl_module/g' _
+  sed -i 's/#LoadModule socache_shmcb_module/LoadModule socache_shmcb_module/g' _
+  sed -i 's/#LoadModule vhost_alias_module/LoadModule vhost_alias_module/g' _
+  sed -i 's/#LoadModule rewrite_module/LoadModule rewrite_module/g' _
+  grep -rl https://overpass-turbo.eu /usr/local/apache2/htdocs | xargs sed -i 's%https://overpass-turbo.eu%https://overpass.openstreetmap.jp/overpass-turbo%g'
+fi
 
 mv _ /usr/local/apache2/conf/httpd.conf
-
-sed -i 's/#LoadModule ssl_module/LoadModule ssl_module/g' /usr/local/apache2/conf/httpd.conf
-sed -i 's/#LoadModule socache_shmcb_module/LoadModule socache_shmcb_module/g' /usr/local/apache2/conf/httpd.conf
-sed -i 's/#LoadModule vhost_alias_module/LoadModule vhost_alias_module/g' /usr/local/apache2/conf/httpd.conf
-sed -i 's/#LoadModule rewrite_module/LoadModule rewrite_module/g' /usr/local/apache2/conf/httpd.conf
-grep -rl https://overpass-turbo.eu /usr/local/apache2/htdocs | xargs sed -i 's%https://overpass-turbo.eu%https://overpass.openstreetmap.jp/overpass-turbo%g'
